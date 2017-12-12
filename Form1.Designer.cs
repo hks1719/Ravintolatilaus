f1c1cd2c53414842e222e2e7d1f5e585919c11a4
Laura Juntunen <laura1738@student.hamk.fi>
2017-12-12 19:47:03 +0200

    Lisäsin Lopetus-painikkeen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,12 +31,13 @@
             this.kokki = new System.Windows.Forms.Button();
             this.tarjoilija = new System.Windows.Forms.Button();
             this.esimies = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // kokki
             // 
             this.kokki.Location = new System.Drawing.Point(22, 35);
-            this.kokki.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.kokki.Margin = new System.Windows.Forms.Padding(2);
             this.kokki.Name = "kokki";
             this.kokki.Size = new System.Drawing.Size(64, 58);
             this.kokki.TabIndex = 0;
@@ -61,17 +62,28 @@
             this.esimies.Text = "Esimies";
             this.esimies.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Lopetus";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(517, 287);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.esimies);
             this.Controls.Add(this.tarjoilija);
             this.Controls.Add(this.kokki);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -81,6 +93,7 @@
         private System.Windows.Forms.Button kokki;
         private System.Windows.Forms.Button tarjoilija;
         private System.Windows.Forms.Button esimies;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,10 @@ namespace Ravintolatilaus
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
