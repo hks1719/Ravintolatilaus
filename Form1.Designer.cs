e88dd6845abb1b94c5ba934dc80152a3fd19ae05
Hanna Solkinen <hanna.solkinen@student.hamk.fi>
2017-12-12 19:40:12 +0200

    muutin painikkeiden kokoa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,41 +35,43 @@
             // 
             // kokki
             // 
-            this.kokki.Location = new System.Drawing.Point(22, 35);
-            this.kokki.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.kokki.Location = new System.Drawing.Point(29, 43);
+            this.kokki.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.kokki.Name = "kokki";
-            this.kokki.Size = new System.Drawing.Size(64, 58);
+            this.kokki.Size = new System.Drawing.Size(85, 71);
             this.kokki.TabIndex = 0;
             this.kokki.Text = "Kokki";
             this.kokki.UseVisualStyleBackColor = true;
             // 
             // tarjoilija
             // 
-            this.tarjoilija.Location = new System.Drawing.Point(125, 35);
+            this.tarjoilija.Location = new System.Drawing.Point(167, 43);
+            this.tarjoilija.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tarjoilija.Name = "tarjoilija";
-            this.tarjoilija.Size = new System.Drawing.Size(75, 23);
+            this.tarjoilija.Size = new System.Drawing.Size(100, 71);
             this.tarjoilija.TabIndex = 1;
             this.tarjoilija.Text = "Tarjoilija";
             this.tarjoilija.UseVisualStyleBackColor = true;
             // 
             // esimies
             // 
-            this.esimies.Location = new System.Drawing.Point(249, 53);
+            this.esimies.Location = new System.Drawing.Point(332, 43);
+            this.esimies.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.esimies.Name = "esimies";
-            this.esimies.Size = new System.Drawing.Size(75, 23);
+            this.esimies.Size = new System.Drawing.Size(100, 71);
             this.esimies.TabIndex = 2;
             this.esimies.Text = "Esimies";
             this.esimies.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(517, 287);
+            this.ClientSize = new System.Drawing.Size(689, 353);
             this.Controls.Add(this.esimies);
             this.Controls.Add(this.tarjoilija);
             this.Controls.Add(this.kokki);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
