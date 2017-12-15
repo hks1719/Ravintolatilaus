3b3948546b61e3f86df3723d4a4370f5efbf18ed
Laura Juntunen <laura1738@student.hamk.fi>
2017-12-15 10:43:26 +0200

    Poyta-taulun haku tietokannasta tehty...jatkoa seuraa...

--- Esimies.cs
+++ Esimies.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,9 +17,69 @@ namespace Ravintolatilaus
             InitializeComponent();
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        // SQL - yhteyden avaus funktio 
+        //public void Avaayhteys()
+        //{
+        //    SqlConnection con = new SqlConnection();
+        //    con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Ravintola.mdf;Integrated Security=True;Connect Timeout=30";
+        //    con.Open();
+        //}    
+
+        // Takaisin päävalikkoon 
+        private void Takaisin_Click(object sender, EventArgs e)
         {
             this.Hide();
         }
+
+        // Hae tilaukset joiden tila on avoinna
+        private void Avoimet_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        // Hae tilaukset joiden tila on valmis
+        private void Valmiit_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        // Tarkastele pöytävaraustilannetta
+        public void Poydat_Click(object sender, EventArgs e)
+        {
+           
+            // Tilapäinen säilytyspaikka 
+            List<string> tempList = new List<string>();
+            // Nollataan lukija
+            SqlDataReader reader = null;
+            // Nollataan käsky
+            SqlCommand cmd = null;
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Ravintola.mdf;Integrated Security=True;Connect Timeout=30";
+            con.Open();
+
+            // SQL-käsky muuttujaan 
+            string CmdText = "SELECT * FROM poyta";
+
+            // SQL-käsky ja yhteys auki
+            cmd = new SqlCommand(CmdText, con);
+
+            // Luetaan tietokantataulun tiedot
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if (!reader.IsDBNull(0))
+                {
+                    tempList.Add(reader[0].ToString() + " " + reader[1].ToString());
+                }
+            }
+            reader.Close();
+            con.Close();
+            // Sijoitetaan String-tyyppiseksi muunnetut tietokantahaun tulokset taulukkoon 
+            string[] poytatilanne = tempList.ToArray();
+
+            // Jatkuu tästä siten että taulukon sisältö saadaan ListanLuku-ikkunaan auki
+        }
     }
-}
+    }
