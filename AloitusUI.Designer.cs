5b72208d9ebdaa7194ed59f356c251844f549be7
Hanna Solkinen <hanna.solkinen@student.hamk.fi>
2017-12-18 17:24:53 +0200

    näytön modifointi

--- AloitusUI.Designer.cs
+++ AloitusUI.Designer.cs
@@ -36,20 +36,24 @@
             // 
             // kokki
             // 
+            this.kokki.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kokki.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.kokki.Location = new System.Drawing.Point(65, 71);
             this.kokki.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.kokki.Name = "kokki";
-            this.kokki.Size = new System.Drawing.Size(85, 85);
+            this.kokki.Size = new System.Drawing.Size(120, 85);
             this.kokki.TabIndex = 0;
             this.kokki.Text = "Kokki";
             this.kokki.UseVisualStyleBackColor = true;
             // 
             // tarjoilija
             // 
+            this.tarjoilija.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tarjoilija.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.tarjoilija.Location = new System.Drawing.Point(249, 71);
-            this.tarjoilija.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tarjoilija.Margin = new System.Windows.Forms.Padding(4);
             this.tarjoilija.Name = "tarjoilija";
-            this.tarjoilija.Size = new System.Drawing.Size(85, 85);
+            this.tarjoilija.Size = new System.Drawing.Size(120, 85);
             this.tarjoilija.TabIndex = 1;
             this.tarjoilija.Text = "Tarjoilija";
             this.tarjoilija.UseVisualStyleBackColor = true;
@@ -57,10 +61,12 @@
             // 
             // pomo
             // 
+            this.pomo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pomo.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.pomo.Location = new System.Drawing.Point(435, 71);
-            this.pomo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pomo.Margin = new System.Windows.Forms.Padding(4);
             this.pomo.Name = "pomo";
-            this.pomo.Size = new System.Drawing.Size(85, 85);
+            this.pomo.Size = new System.Drawing.Size(120, 85);
             this.pomo.TabIndex = 2;
             this.pomo.Text = "Esimies";
             this.pomo.UseVisualStyleBackColor = true;
@@ -68,8 +74,10 @@
             // 
             // Sulje
             // 
+            this.Sulje.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sulje.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.Sulje.Location = new System.Drawing.Point(65, 268);
-            this.Sulje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Sulje.Margin = new System.Windows.Forms.Padding(4);
             this.Sulje.Name = "Sulje";
             this.Sulje.Size = new System.Drawing.Size(116, 41);
             this.Sulje.TabIndex = 3;
